55120b07b3a5042f8cb9e3d996c9758522a2ea6f
Philip Thomsen <philip-th@live.dk>
2022-08-10 14:19:17 +0200

    tilføjet kode igen

---
+++ js/script.js
@@ -0,0 +1,50 @@
+let option = document.querySelector('#operator')
+const bTn = document.querySelector('#bTn')
+const numberOne = document.querySelector('#numberOne')
+const numberTwo = document.querySelector('#numberTwo')
+let errors = [
+  "GxM3wstBcD4",
+  "-8lDYrvTILc",
+]
+console.log(errors[Math.floor(Math.random() * errors.length)]);
+change()
+function udregn() {
+  if (option.value == "/") {
+    if (parseInt(numberOne.value) == 0 || parseInt(numberTwo.value) == 0) {
+    document.querySelector('#fieldset').style.display = "none"
+    document.querySelector('#myDiv').style.display = "block"
+    
+    document.querySelector('#myIframe').src = "https://youtube.com/embed/" + errors[Math.floor(Math.random() * errors.length)] + "?autoplay=1"
+    interval = setInterval(() => {
+      document.querySelector('#fieldset').style.display = "block"
+      document.querySelector('#myDiv').style.display = "none"
+      document.querySelector('#myIframe').src = ""
+      document.body.style.backgroundColor = "white"
+      clearInterval(interval)
+    }, 30000);
+  }
+  
+    
+  }else{
+    if (option.value == "+") {
+      console.log(parseInt(numberOne.value) + parseInt(numberTwo.value));  
+    }
+    if (option.value == "-") {
+        console.log(parseInt(numberOne.value) - parseInt(numberTwo.value));  
+      }
+      if (option.value == "*") {
+        console.log(parseInt(numberOne.value) * parseInt(numberTwo.value));  
+      }
+      if (option.value == "/") {
+        console.log(parseInt(numberOne.value) / parseInt(numberTwo.value));  
+      }
+  }
+    
+    
+}
+function change() {
+    document.querySelector('#shownOperator').innerHTML = option.value
+}
+bTn.addEventListener('click', function(){
+    udregn()
+})
