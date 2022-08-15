f4265b8cb7b8fd5f8193614bccbe4b85461f1b7f
Philip Thomsen <philip-th@live.dk>
2022-08-15 12:50:33 +0200

    Små ændringer
    Fjernet tilfældig video og input nummer 2

--- js/script.js
+++ js/script.js
@@ -1,44 +1,9 @@
 let option = document.querySelector('#operator')
 const bTn = document.querySelector('#bTn')
 const numberOne = document.querySelector('#numberOne')
-const numberTwo = document.querySelector('#numberTwo')
-let errors = [
-  "GxM3wstBcD4",
-  "-8lDYrvTILc",
-]
-console.log(errors[Math.floor(Math.random() * errors.length)]);
+
 change()
 function udregn() {
-  if (option.value == "/") {
-    if (parseInt(numberOne.value) == 0 || parseInt(numberTwo.value) == 0) {
-    document.querySelector('#fieldset').style.display = "none"
-    document.querySelector('#myDiv').style.display = "block"
-    
-    document.querySelector('#myIframe').src = "https://youtube.com/embed/" + errors[Math.floor(Math.random() * errors.length)] + "?autoplay=1"
-    interval = setInterval(() => {
-      document.querySelector('#fieldset').style.display = "block"
-      document.querySelector('#myDiv').style.display = "none"
-      document.querySelector('#myIframe').src = ""
-      clearInterval(interval)
-    }, 30000);
-  }
-  
-    
-  }else{
-    if (option.value == "+") {
-      console.log(parseInt(numberOne.value) + parseInt(numberTwo.value));  
-    }
-    if (option.value == "-") {
-        console.log(parseInt(numberOne.value) - parseInt(numberTwo.value));  
-      }
-      if (option.value == "*") {
-        console.log(parseInt(numberOne.value) * parseInt(numberTwo.value));  
-      }
-      if (option.value == "/") {
-        console.log(parseInt(numberOne.value) / parseInt(numberTwo.value));  
-      }
-  }
-    
     
 }
 function change() {
