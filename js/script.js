9e51240a43a21051447fb4c71d0d42a4b2ea3d4f
Nicekode4 <philip-th@live.dk>
2022-08-17 21:50:01 +0200

    Calculator upgrade
    Calculator is not more advanced and can calc everything on one line

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
-let option = document.querySelector('#operator')
+
 const bTn = document.querySelector('#bTn')
 const numberOne = document.querySelector('#numberOne')
-
-change()
 function udregn() {
-    
+    let value = numberOne.value
+    value = value.split(",").join(".")
+    console.log(eval(value));
+    document.querySelector('#result').innerHTML = eval(value)
 }
-function change() {
-    document.querySelector('#shownOperator').innerHTML = option.value
+function add(obj) {
+    numberOne.value = numberOne.value + obj.innerHTML
 }
-bTn.addEventListener('click', function(){
-    udregn()
-})
